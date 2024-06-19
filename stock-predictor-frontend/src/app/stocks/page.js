9af4e4906e462e41fe36fec5c45cebfaf78b6e7b
KajtekWisniewski <k.wisniewski.603@studms.ug.edu.pl>
2024-06-19 18:14:34 +0200

    added navbar to layout

--- stock-predictor-frontend/src/app/stocks/page.js
+++ stock-predictor-frontend/src/app/stocks/page.js
@@ -6,9 +6,6 @@ import styles from '../stockStyles.module.scss';
 export default function StocksPage() {
   return (
     <div className={styles.mainDiv}>
-      <header className="App-header">
-        <h1>My Trading Dashboard</h1>
-      </header>
       <div className={styles.stockWidget}>
         <TradingViewWidget />
       </div>
