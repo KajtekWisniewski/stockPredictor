1646fd869d5426692e8a2559de305d0e1035b5fb
KajtekWisniewski <k.wisniewski.603@studms.ug.edu.pl>
2024-06-20 16:40:12 +0200

    added stock entity and dto

---
+++ AuthService/AuthService/DTOs/StockDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthService.DTOs
+{
+    public class StockDto
+    {
+
+        public int Id { get; init; }
+        public string Ticker { get; init; } = "";
+        public string DateOfPrediction { get; init; } = ""; 
+        public float PredictedReturn { get; init; }
+        public int DaysPredicted { get; init; } 
+    }
+}
